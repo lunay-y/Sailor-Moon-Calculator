e6ede9fba7c00cc815e175a7a4b0592e4b9c75d3
Luna Yu <yuanluna.yu@hotmail.it>
2022-04-27 17:08:33 +0100

    added operators buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import Button from "../Button/Button.js";
 import Calculator from "../Calculator/Calculator.js";
+import { useState } from "react";
 
 function App() {
+   const [currentCalculation, setCalculation] = useState([]);
+
+   function calculation(num) {
+      setCalculation([...currentCalculation, num]);
+   }
+
+   function calculateTotal() {
+      return [...currentCalculation];
+   }
    return (
       <div className="App">
          <Button text="hey" />
-         <Calculator />
+         <Calculator
+            calculation={calculation}
+            total={currentCalculation}
+            calculateTotal={calculateTotal}
+         />
       </div>
    );
 }
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,15 +2,29 @@ import React from "react";
 import Button from "../Button/Button.js";
 import css from "./Calculator.module.css";
 
-function Calculator({ total }) {
-   const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, ".", "="];
+function Calculator({ calculation, total, calculateTotal }) {
+   const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+   const operators = ["/", "*", "-", "+"];
    return (
       <div className={css.Calculator}>
          <h1>{total}</h1>
          <div className={css.numbersContainer}>
             {numbers.map(function (number) {
-               return <Button key={number} text={number} />;
+               return (
+                  <Button
+                     key={number}
+                     text={number}
+                     onClick={() => calculation(number)}
+                  />
+               );
             })}
+            <Button text="." />
+            <Button text="=" onClick={calculateTotal} />
+            <div className={css.operators}>
+               {operators.map(function (operator) {
+                  return <Button key={operator} text={operator} />;
+               })}
+            </div>
          </div>
       </div>
    );
