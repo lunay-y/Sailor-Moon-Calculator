9fbc29fd171cb0065fe343aa0100e670a9cae79f
Luna Yu <yuanluna.yu@hotmail.it>
2022-04-27 12:04:50 +0100

    create global button

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "../Button/Button.js";
+
+function App() {
+   return (
+      <div className="App">
+         <Button text="hey" />
+      </div>
+   );
+}
+
+export default App;
