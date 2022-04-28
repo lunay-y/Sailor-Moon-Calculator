feaf90471f94679d35d6e36ae425dc6bd45599b4
Luna Yu <yuanluna.yu@hotmail.it>
2022-04-28 17:04:24 +0100

    styled calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,30 +1,40 @@
 import React from "react";
 import Button from "../Button/Button.js";
 import css from "./Calculator.module.css";
+//import { useReducer } from "react";
+
+//function reducer() {}
 
 function Calculator({ total, calculateTotal }) {
+   //const [{}, dispatch] = useReducer(reducer, {});
    return (
-      <div className={css.Calculator}>
-         <h5>hey</h5>
-         <h2>{total}</h2>
-         <div className={css.numbersContainer}>
-            <Button text="7" />
-            <Button text="8" />
-            <Button text="9" />
-            <Button text="4" />
-            <Button text="5" />
-            <Button text="6" />
-            <Button text="1" />
-            <Button text="2" />
-            <Button text="3" />
-            <Button text="0" />
-            <Button text="." />
-            <Button text="=" onClick={calculateTotal} />
-            <Button text="÷" />
-            <Button text="*" />
-            <Button text="-" />
-            <Button text="+" />
+      <div className={css.calculator}>
+         <div className={css.output}>
+            <h5 className={css.previousOperation}>hey</h5>
+            <h2 className={css.currentOperation}>{total}</h2>
          </div>
+         <Button text="AC" className={css.spanGridTwo} />
+         <Button text="DEL" />
+         <Button text="÷" />
+         <Button text="1" />
+         <Button text="2" />
+         <Button text="3" />
+         <Button text="*" />
+         <Button text="4" />
+         <Button text="5" />
+         <Button text="6" />
+         <Button text="+" />
+         <Button text="7" />
+         <Button text="8" />
+         <Button text="9" />
+         <Button text="-" />
+         <Button text="." />
+         <Button text="0" />
+         <Button
+            text="="
+            className={css.spanGridTwo}
+            onClick={calculateTotal}
+         />
       </div>
    );
 }
